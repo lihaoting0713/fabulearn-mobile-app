da3c1307acdbf7151e973b55d334afc4ec4d3917
lihaoting0713 <haotingli2000@outlook.com>
2024-06-30 22:52:09 +0800

    rebuilt index.js

--- app/index.js
+++ app/index.js
@@ -5,10 +5,12 @@ import Layout from './HomeScreen'; // Make sure to import the Layout from _layou
 import { Provider } from 'react-redux';
 import store from './Store'; // Ensure correct import path
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
 import StudyPackageStack from './StudyPackageStack'; // Ensure correct import path
 import HomeScreen from './HomeScreen'; // Ensure correct import path
-
+import VideoStack from './VideoStack';
+import Notification from './Notifications';
+import AccountStack from './AccountStack';
+import ChallengeStack from './ChallengeStack';
 
 
 const MainStack = createNativeStackNavigator();
@@ -18,7 +20,11 @@ export default function Index() {
     <Provider store={store}>
       <MainStack.Navigator screenOptions={{ headerShown: false }}>
       <MainStack.Screen name="HomeScreen" component={HomeScreen} />
+      <MainStack.Screen name="VideoStack" component={VideoStack} />
       <MainStack.Screen name="StudyPackageStack" component={StudyPackageStack} />
+      <MainStack.Screen name="Notification" component={Notification} />
+      <MainStack.Screen name="AccountStack" component={AccountStack} />
+      <MainStack.Screen name="ChallengeStack" component={ChallengeStack} />
       </MainStack.Navigator>
     </Provider>
   );
